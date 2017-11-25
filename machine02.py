3d22ceff7c62d583fc7d49c6ed7100564643c460
alexmasluk <alex.masluk@gmail.com>
2017-11-25 13:52:46 -0800

    add experiment 4, 5

--- machine02.py
+++ machine02.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 #import scipy.io as sio
 
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import roc_curve, roc_auc_score, zero_one_loss
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 import numpy as np
 
 def load_data():
@@ -57,7 +58,6 @@ def load_data():
                 index = category_lists[header].index(feat)
                 new_feat.append(index)
         data2.append(new_feat)
-
     dataset = data2
 
     # use OneHotEncoder to replace categorical values with binary vectors
@@ -70,8 +70,14 @@ def load_data():
         data2.append(arr)
 
     dataset = data2
-
     return dataset, response
+
+
+def reshape(d):
+    data = np.array(d)
+    data = np.array(data)
+    nsamples, nx, ny = data.shape
+    return data.reshape(nsamples, nx*ny)
 
 def split_data(data, response):
     '''Experiment 1
@@ -80,23 +86,17 @@ def split_data(data, response):
     X_train, X_test, y_train, y_test = train_test_split(
             data, response, test_size = 0.25)
 
-    return (X_train, X_test), (y_train, y_test)
-
-def reshape(d):
-    data = np.array(d)
-    data = np.array(data)
-    nsamples, nx, ny = data.shape
-    return data.reshape(nsamples, nx*ny)
+    return (reshape(X_train), reshape(X_test)), (y_train, y_test)
 
 
 def gaussian_nb(data, response):
     clf = GaussianNB()
-    data = reshape(data)
     clf.fit(data, np.array(response).ravel())
     return clf
 
-def test_gauss(clf, data, response):
+def test_nb(clf, data, response):
     good, bad = 0,0
+    '''
     for i, x in enumerate(data):
         pred = clf.predict(x)
         actual = response[i]
@@ -105,8 +105,9 @@ def test_gauss(clf, data, response):
         else:
             bad += 1
     print("good {} bad {}".format(good, bad))
+    '''
 
-    scores = clf.predict(reshape(data))
+    scores = clf.predict(data)
     fpr, tpr, thresholds = roc_curve(response, scores, pos_label=1)
     roc_score = roc_auc_score(response, scores)
     print("FPR",fpr)
@@ -115,9 +116,8 @@ def test_gauss(clf, data, response):
     print("AUC",roc_score)
 
 def knn(data, response):
-    data = reshape(data)
     print(len(data))
-    n = int(np.sqrt(len(data)))
+    n = int(np.sqrt(len(data))) + 1
     k_classifiers = []
     for x in range(5, n):
         k = KNeighborsClassifier(n_neighbors=x)
@@ -126,6 +126,44 @@ def knn(data, response):
 
     return k_classifiers
 
+def test_classifiers(classifiers, data, response):
+    scores = []
+    for c in classifiers:
+        pred = c.predict(data)
+        scores.append(zero_one_loss(response, pred))
+
+    for x, score in enumerate(scores):
+        print( (x+5), score )
+
+    print(min(scores))
+
+def svm(data, response):
+    max_it = -1
+    v=False
+    clf_l, clf_p3, clf_p5 = SVC(kernel = "linear", verbose=v, max_iter=max_it), SVC(
+            kernel = "poly", verbose=v, max_iter=max_it), SVC(
+                    kernel = "poly", degree=5, verbose=v, max_iter=max_it)
+    print("fitting with kernel=linear")
+    clf_l.fit(data, response)
+
+    print("fitting with kernel=poly, degree 3")
+    clf_p3.fit(data, response)
+
+    print("fitting with kernel=poly, degree 5")
+    clf_p5.fit(data, response)
+    return [clf_l, clf_p3, clf_p5]
+
+
+def standardize(data):
+    # standardize
+    d1, d2 = data[0], data[1]
+    s1, s2 = StandardScaler(), StandardScaler()
+    s1.fit(d1)
+    s2.fit(d2)
+    d1 = s1.transform(d1)
+    d2 = s2.transform(d2)
+    data = (d1, d2)
+    return data
 
 def main():
     # setup
@@ -143,17 +181,26 @@ def main():
 
     # experiment 3
     print("Experiment 3: Use the classifier to predict labels, and eval performance")
-    test_gauss(clf, data[1], response[1])
+    test_nb(clf, data[1], response[1])
     print("Experiment 3 complete\n")
 
 
     # experiment 4
     print("Experiment 4: Fit KNN classifier to data, evaluate")
-    klassifiers = knn(data[0], response[0])
-    print(len(klassifiers))
-
+    k_classifiers = knn(data[0], response[0])
+    test_classifiers(k_classifiers, data[1], response[1])
     print("Experiment 4 complete\n")
 
+    # experiment 5
+    #TODO implement experiment 5, choose best between knn vs naive bayes
+
+    # experiment 3
+    print("Experiment 6: Fit SVM classifier to data, evaluate")
+    # standardize for svm
+    data = standardize(data)
+    svms = svm(data[0], response[0])
+    test_classifiers(svms, data[1], response[1])
+    print("Experiment 6 complete\n")
 
 
 if __name__ == '__main__':
