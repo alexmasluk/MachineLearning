882560ce81dec0d443f6c3a06957c0c548d7cb9f
alexmasluk <alex.masluk@gmail.com>
2017-12-03 19:52:33 -0800

    minor reorg

---
+++ p2/machine02.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+#import scipy.io as sio
+
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve, roc_auc_score, zero_one_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import numpy as np
+
+def load_data():
+    '''Load and pre-process the data
+    '''
+    with open('human.csv') as f:
+        data = f.readlines()
+
+
+    # the dataset's first row is the column headers, ignoring ID column
+    column_headers = data[0].strip().split(',')[1:]
+    num_columns = len(column_headers)
+
+    # dictionary needed to accumulate values for qual. vars
+    category_lists = {}
+    for heading in column_headers:
+        category_lists[heading] = []
+
+    # format the data as a list of lists
+    dataset  = []
+    response = []
+    for datum in data[1:]:
+        features = datum.strip().split(',')[1:]
+        dataset.append(features[:-1])
+        response.append(float(features[-1:][0]))
+
+        # track list of qual. var values
+        for x, feature in enumerate(features):
+            category_lists[column_headers[x]].append(feature)
+
+
+    # remove duplicate values
+    for header in column_headers:
+        category_lists[header] = list(set(category_lists[header]))
+
+    # replace qual. vars with indices
+    data2 = []
+    c_feat = []
+    for datum in dataset:
+        new_feat = []
+        for x, feat in enumerate(datum):
+            header = column_headers[x]
+            try:
+                f_val = float(feat)
+                new_feat.append(f_val)
+            except ValueError:
+                if x not in c_feat:
+                    c_feat.append(x)
+                index = category_lists[header].index(feat)
+                new_feat.append(index)
+        data2.append(new_feat)
+    dataset = data2
+
+    # use OneHotEncoder to replace categorical values with binary vectors
+    enc = OneHotEncoder(categorical_features = c_feat)
+    enc.fit(dataset)
+
+    data2 = []
+    for dat in dataset:
+        arr = enc.transform([dat]).toarray()
+        data2.append(arr)
+
+    dataset = data2
+    return dataset, response
+
+
+def reshape(d):
+    data = np.array(d)
+    data = np.array(data)
+    nsamples, nx, ny = data.shape
+    return data.reshape(nsamples, nx*ny)
+
+def split_data(data, response):
+    '''Experiment 1
+    Use train_test_split to split in to training, test
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(
+            data, response, test_size = 0.25)
+
+    return (reshape(X_train), reshape(X_test)), (y_train, y_test)
+
+
+def gaussian_nb(data, response):
+    clf = GaussianNB()
+    clf.fit(data, np.array(response).ravel())
+    return clf
+
+def test_nb(clf, data, response):
+    good, bad = 0,0
+    '''
+    for i, x in enumerate(data):
+        pred = clf.predict(x)
+        actual = response[i]
+        if pred == actual:
+            good += 1
+        else:
+            bad += 1
+    print("good {} bad {}".format(good, bad))
+    '''
+
+    scores = clf.predict(data)
+    fpr, tpr, thresholds = roc_curve(response, scores, pos_label=1)
+    roc_score = roc_auc_score(response, scores)
+    print("FPR",fpr)
+    print("TPR",tpr)
+    print("THR",thresholds)
+    print("AUC",roc_score)
+
+def knn(data, response):
+    print(len(data))
+    n = int(np.sqrt(len(data))) + 1
+    k_classifiers = []
+    for x in range(5, n):
+        k = KNeighborsClassifier(n_neighbors=x)
+        k.fit(data, response)
+        k_classifiers.append(k)
+
+    return k_classifiers
+
+def test_classifiers(classifiers, data, response):
+    scores = []
+    for c in classifiers:
+        pred = c.predict(data)
+        scores.append(zero_one_loss(response, pred))
+
+    for x, score in enumerate(scores):
+        print( (x+5), score )
+
+    print(min(scores))
+
+def svm(data, response):
+    max_it = -1
+    v=False
+    clf_l, clf_p3, clf_p5 = SVC(kernel = "linear", verbose=v, max_iter=max_it), SVC(
+            kernel = "poly", verbose=v, max_iter=max_it), SVC(
+                    kernel = "poly", degree=5, verbose=v, max_iter=max_it)
+    print("fitting with kernel=linear")
+    clf_l.fit(data, response)
+
+    print("fitting with kernel=poly, degree 3")
+    clf_p3.fit(data, response)
+
+    print("fitting with kernel=poly, degree 5")
+    clf_p5.fit(data, response)
+    return [clf_l, clf_p3, clf_p5]
+
+
+def standardize(data):
+    # standardize
+    d1, d2 = data[0], data[1]
+    s1, s2 = StandardScaler(), StandardScaler()
+    s1.fit(d1)
+    s2.fit(d2)
+    d1 = s1.transform(d1)
+    d2 = s2.transform(d2)
+    data = (d1, d2)
+    return data
+
+def main():
+    # setup
+    data, response = load_data()
+
+    # experiment 1
+    print("Experiment 1: Split into test and training")
+    data, response = split_data(data, response)
+    print("Experiment 1 complete\n")
+
+    # standardize for svm
+    data = standardize(data)
+
+    # experiment 2
+    print("Experiment 2: Fit GaussianNB classifier to data")
+    clf = gaussian_nb(data[0], response[0])
+    print("Experiment 2 complete\n")
+
+    # experiment 3
+    print("Experiment 3: Use the classifier to predict labels, and eval performance")
+    test_nb(clf, data[1], response[1])
+    print("Experiment 3 complete\n")
+
+
+    # experiment 4
+    print("Experiment 4: Fit KNN classifier to data, evaluate")
+    k_classifiers = knn(data[0], response[0])
+    test_classifiers(k_classifiers, data[1], response[1])
+    print("Experiment 4 complete\n")
+
+    # experiment 5
+    #TODO implement experiment 5, choose best between knn vs naive bayes
+
+    # experiment 3
+    print("Experiment 6: Fit SVM classifier to data, evaluate")
+    svms = svm(data[0], response[0])
+    test_classifiers(svms, data[1], response[1])
+    print("Experiment 6 complete\n")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ p2/machine02b.py
@@ -0,0 +1,270 @@
+#!/usr/bin/python
+#import scipy.io as sio
+
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve, roc_auc_score, zero_one_loss, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+from sklearn.svm import SVC
+from itertools import permutations
+import numpy as np
+
+def load_data():
+    '''Load and pre-process the data
+    '''
+    print("loading data")
+    with open('human.csv') as f:
+        data = f.readlines()
+
+
+    # the dataset's first row is the column headers, ignoring ID column
+    column_headers = data[0].strip().split(',')[1:]
+    num_columns = len(column_headers)
+
+    # format the data as a list of lists keep labels separate
+    dataset  = []
+    response = []
+    x = 0
+
+    for datum in data:
+        #print(x)
+        features = datum.strip().split(',')[1:]
+        dataset.append(features[:-1])
+        response.append(int(features[-1:][0]) - 1)
+        x += 1
+    return np.array(dataset).astype(np.float), response
+
+def split_data(data, response):
+    '''Experiment 1
+    Use train_test_split to split in to training, test
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(
+            data, response, test_size = 0.25)
+
+    return (X_train, X_test), (y_train, y_test)
+
+
+def gaussian_nb(data, response):
+    '''Experiment 2
+    Fit gaussian naive bayes classifier to data
+    '''
+    clf = GaussianNB()
+    clf.fit(data, np.array(response).ravel())
+    return clf
+
+
+def test_nb(clf, data, response):
+    '''Experiment 3
+    Test naive bayes classifier
+    Display confusion matrix
+    Return average loss for comparison with other classifiers
+    '''
+    predict = clf.predict(data)
+    c = confusion_matrix(response, predict)
+    print("CONFUSION MATRIX")
+    print(c)
+    FP = c.sum(axis=0) - np.diag(c)  
+    FN = c.sum(axis=1) - np.diag(c)
+    TP = np.diag(c)
+    TN = c.sum() - (FP + FN + TP)
+    
+    performance = np.mean((TP+TN)/(TP+FP+FN+TN))
+
+    print("performance: {}".format(performance))
+    return (clf, performance)
+
+
+def knn(data, response):
+    '''Experiment 4
+    Fit K Neighbors classifiers to data
+    NOTE we did not use range k=5 to sqrt(N) due to large N and high dimensionality
+    Please see separate text file for output of testing all values of K
+    '''
+    n = int(np.sqrt(len(data))) + 1
+    print(len(data), n)
+    k_classifiers = []
+    for x in range(3, 6):
+        print('fitting knn with k = {}'.format(x))
+        k = KNeighborsClassifier(n_neighbors=x)
+        k.fit(data, response)
+        k_classifiers.append(k)
+
+    return k_classifiers
+
+def test_classifiers(classifiers, data, response):
+    '''Experiment 5, 7
+    Test classifier performance with zero_one_loss
+    '''
+
+    scores = []
+    for c in classifiers:
+        print("testing classifier: {}".format(c))
+        pred = c.predict(data)
+        scores.append( (c, zero_one_loss(response, pred)) )
+
+    for score in scores:
+        print(score)
+
+    return scores
+
+def svm(data, response):
+    '''Experiment 6
+    Fit 3 svm models to data
+    Kernel = linear, kernel = poly with deg 3, kernel = poly with degree 5
+    '''
+    max_it = -1
+    v=False
+    clf_l, clf_p3, clf_p5 = SVC(kernel = "linear", verbose=v, max_iter=max_it), SVC(
+            kernel = "poly", verbose=v, max_iter=max_it), SVC(
+                    kernel = "poly", degree=5, verbose=v, max_iter=max_it)
+    print("fitting with kernel=linear")
+    clf_l.fit(data, response)
+
+    print("fitting with kernel=poly, degree 3")
+    clf_p3.fit(data, response)
+
+    print("fitting with kernel=poly, degree 5")
+    clf_p5.fit(data, response)
+    return [clf_l, clf_p3, clf_p5]
+
+def kmeans(data):
+    '''Experiment 8
+    use k means clustering to cluster the dataset
+    return the fit model so we can determine if clustering corresponds to class labels
+    '''
+    km = KMeans(n_clusters = 6)
+    km.fit(data)
+    return km
+
+def test_km(pred, actual):
+    '''Experiment 8
+    Our k-means didn't necessarily label the classes in the same order, so
+    we'll score all possible class order permutations and use the best one to determine
+    how accurately it found clusters that correspond to our actual classes
+    '''
+    mappings = list(permutations(['a','b','c','d','e','f']))
+    l_to_d = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5}
+
+    best_score = 1.0
+
+
+    for mapping in mappings:
+        d_to_l = {}
+        for x, m in enumerate(mapping):
+            d_to_l[x] = m
+        remapped_pred = [l_to_d[d_to_l[p]] for p in pred]
+        '''
+        for p in pred:
+            remapped_pred.append(l_to_d[d_to_l[p]])
+            '''
+        score = zero_one_loss(remapped_pred, actual)
+        if score < best_score:
+            best_score = score
+
+    return best_score
+
+
+def pick_best_classifier(scores):
+    '''Choose the highest-performing classifier from a list
+    Return the classifier and its score
+    '''
+    best = 1.0
+    best_c = None
+    for score in scores:
+        if score[1] < best:
+            best = score[1]
+            best_c = score[0]
+
+    return (best_c, best)
+
+def main():
+    # setup
+    data, response = load_data()
+    scores = []
+
+    # experiment 1
+    print("Experiment 1: Split into test and training")
+    data, response = split_data(data, response)
+    print("Experiment 1 complete\n")
+    input("Press enter to continue . . .")
+
+    # standardize for svm
+    #  ** our dataset is already standardized **
+    #data = standardize(data)
+
+    # experiment 2
+    print("Experiment 2: Fit GaussianNB classifier to data")
+    clf = gaussian_nb(data[0], response[0])
+    print("Experiment 2 complete\n")
+    input("Press enter to continue . . .")
+
+    # experiment 3
+    # OUTPUT: 
+    # CONFUSION MATRIX
+    #[[326  69  36   0   0   0]
+    # [ 13 352  23   0   0   0]
+    # [ 16  56 224   0   0   0]
+    # [  0   4   0 425  29   3]
+    # [  0   8   0 325 141   0]
+    # [  0  15   0  19   0 491]]
+    #performance: 0.9202588996763755
+    print("Experiment 3: Use the classifier to predict labels, and eval performance")
+    scores.append(test_nb(clf, data[1], response[1]))
+    print("Experiment 3 complete\n")
+    input("Press enter to continue . . .")
+
+
+    # experiment 4
+    # OUTPUT: 
+    # K=3: 0.028349514563106748
+    # K=4: 0.038058252427184414
+    # K=5: 0.034174757281553392
+    print("Experiment 4: Fit KNN classifier to data, evaluate")
+    k_classifiers = knn(data[0], response[0])
+    print("Testing KNN classifiers..")
+    scores.extend(test_classifiers(k_classifiers, data[1], response[1]))
+    print("Experiment 4 complete\n")
+    input("Press enter to continue . . .")
+
+    # experiment 5
+    # OUTPUT: 0.028349514563106748
+    print("Experiment 5: Which is better, our best KNN classifier or our Naive Bayes?")
+    best = pick_best_classifier(scores)
+    print(best[0], best[1])
+    print("Experiment 5 complete\n")
+    input("Press enter to continue . . .")
+
+    # experiment 6
+    # OUTPUT: 
+    # Linear: 0.01281553398058255
+    # Degree 3: 0.073009708737864054
+    # Degree 5: 0.15339805825242714
+    print("Experiment 6: Fit SVM classifier to data, evaluate")
+    svms = svm(data[0], response[0])
+    scores.extend(test_classifiers(svms, data[1], response[1]))
+    print("Experiment 6 complete\n")
+    input("Press enter to continue . . .")
+
+    # experiment7
+    # OUTPUT: 0.01281553398058255
+    print("Experiment 7: Which is better, our best KNN classifier or our Naive Bayes?")
+    best = pick_best_classifier(scores)
+    print(best[0], best[1])
+    print("Experiment 7 complete\n")
+    input("Press enter to continue . . .")
+
+    # experiment 8
+    # OUTPUT: 0.399792853444 loss for k means
+    print("Experiment 8: Remove labels, use KMeans to cluster data")
+    km = kmeans(data[0])
+    score = test_km(list(km.labels_), response[0])
+    print("{} loss for k means".format(score))
+    print("Experiment 8 complete")
+    input("Press enter to continue . . .")
+
+
+if __name__ == '__main__':
+    main()
+
